e7ca073b99835e7733ef2c59bac8dca7d4bc7c1d
jackyliu16 <18922251299@163.com>
2024-11-08 01:30:03 +0800

    copy from https://atomgit.com/onedragon424/rust-alg/

--- exercises/solution4/src/rec_mc.rs
+++ exercises/solution4/src/rec_mc.rs
@@ -1,4 +1,14 @@
 pub fn dp_rec_mc(amount: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    let coin_price: [usize; 8] = [1, 2, 5, 10, 20, 30, 50, 100];
+    let mut dp = vec![std::usize::MAX; amount as usize + 1]; // Currently optimal number of money
+    dp[0] = 0;
+
+    // dp[i] = min(dp[i], dp[i - coin[j]] + 1)
+    for i in 0..=amount as usize {
+        for j in coin_price.iter().rev().filter(|&&x| x <= amount as usize && x <= i) {
+            dp[i as usize] = std::cmp::min(dp[i], dp[i - j] + 1);
+        }
+    }
+
+    dp[amount as usize] as u32
 }
