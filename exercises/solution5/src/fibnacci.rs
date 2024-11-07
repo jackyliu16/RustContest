fb0003f7023c092d47d329a7c57db5137067ac2f
jackyliu16 <18922251299@163.com>
2024-11-08 01:30:29 +0800

    copy from https://atomgit.com/onedragon424/rust-alg/

--- exercises/solution5/src/fibnacci.rs
+++ exercises/solution5/src/fibnacci.rs
@@ -1,4 +1,19 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    // 其实 fibnacci sum 的减半就相当于结果（在最后一位为偶数的情况）
+    let mut prev: u32 = 1;
+    let mut curr: u32 = 1;
+    let mut res = 1; // first one
+    let mut flag: bool = true;
+
+    // Something like sliding window, only contains the contents calculating.
+    while curr < threshold {
+        if curr % 2 != 0 {
+            res += curr;
+            dbg!(curr);
+        }
+        curr += prev;
+        prev = curr - prev;
+    }
+
+    res
 }
