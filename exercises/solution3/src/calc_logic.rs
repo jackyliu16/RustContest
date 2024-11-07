37dd4902b1e8d421c25234e5ebbe7a3d57edfab1
jackyliu16 <18922251299@163.com>
2024-11-08 01:29:35 +0800

    copy from https://atomgit.com/onedragon424/rust-alg/

--- exercises/solution3/src/calc_logic.rs
+++ exercises/solution3/src/calc_logic.rs
@@ -1,4 +1,13 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    if n > 365 {
+        return 1.0_f64;
+    }
+
+    let mut poss = 1.0_f64;                     // 1 - P(没有人在同一天过生日)
+
+    for i in 0..n {
+        poss *= (365 - i) as f64 / 365.0_f64;       // FIXME: four-digit decimation
+    }
+
+    1.0_f64 - poss
 }
