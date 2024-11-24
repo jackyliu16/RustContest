71eed365da56f2f6d770b273c83b20ce92884d7a
jackyliu16 <18922251299@163.com>
2024-11-25 07:16:33 +0800

    topic1(S7): impl identity_card

--- topic1/solutiont7/src/identity_card.rs
+++ topic1/solutiont7/src/identity_card.rs
@@ -1,8 +1,26 @@
 use std::fmt::Display;
 use std::str::FromStr;
+use super::gb11643_1989::GB11643_1989;
+use super::gb11643_1999::GB11643_1999;
+use serde_json::to_string;
+use crate::error::{ParseError};
 
 pub fn check_id_card(id_card_no: &str) -> String {
-    String::new()
+    match id_card_no.len() {
+        15 => {
+            if let Ok(gb) = id_card_no.parse::<GB11643_1989>() {
+                format!("身份证号码正确,{}", gb.to_string())
+            } else {
+           String::from("身份证号码错误")
+        }
+        },
+        _ => {
+            if let Ok(gb) = id_card_no.parse::<GB11643_1999>() {
+                format!("身份证号码正确,{}", gb.to_string())
+            } else {
+                String::from("身份证号码错误")
+            }
+        }
+    }
 }
-
-pub trait GB11643: FromStr + Display {}
+pub trait GB11643: FromStr + Display { }
