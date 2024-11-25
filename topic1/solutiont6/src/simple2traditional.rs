b06451f3157d38871764493ed19bc3f0018dd0a9
jackyliu16 <18922251299@163.com>
2024-11-25 18:07:18 +0800

    temp

--- topic1/solutiont6/src/simple2traditional.rs
+++ topic1/solutiont6/src/simple2traditional.rs
@@ -31,31 +31,40 @@ impl FromStr for TranslationType {
 }
 
 fn get_map_from_file(types: TranslationType) -> io::Result<HashMap<String, String>> {
-    let file = match types {
-        TranslationType::Simple2Traditional => { "STCharacters.txt" },
-        TranslationType::Traditional2Simple => { "TSCharacters.txt" },
+    let files = match types {
+        TranslationType::Simple2Traditional => { vec![
+            "STCharacters.txt",
+            "STPhrases.txt",
+        ]},
+        TranslationType::Traditional2Simple => { vec![
+            "TSCharacters.txt",
+            "TSPhrases.txt"
+        ]},
     };
     let manifest_path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let path = PathBuf::from(manifest_path).join("dict").join(file);
-    dbg!(&path);
-
     let mut map: HashMap<String, String> = HashMap::new();
-    let mut file = File::open(path)?;
+    for file in files {
+        let path = PathBuf::from(&manifest_path).join("dict").join(file);
+        dbg!(&path);
 
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        let line = line?;
-        let line = line.split_whitespace().collect::<Vec<&str>>();
-        let s = line[0];
-        let t = if line.len() == 3 {
-            line[2]
-        } else {
-            line[1]
-        };
-        match types {
-            TranslationType::Traditional2Simple => { map.insert(t.to_string(), s.to_string()); }
-            TranslationType::Simple2Traditional => { map.insert(s.to_string(), t.to_string()); }
+        let mut file = File::open(path)?;
+
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let line = line?;
+            let line = line.split_whitespace().collect::<Vec<&str>>();
+            let s = line[0];
+            let t = if line.len() == 3 {
+                line[2]
+            } else {
+                line[1]
+            };
+            match types {
+                TranslationType::Traditional2Simple => { map.insert(t.to_string(), s.to_string()); }
+                TranslationType::Simple2Traditional => { map.insert(s.to_string(), t.to_string()); }
+            }
         }
+
     }
 
     Ok(map)
