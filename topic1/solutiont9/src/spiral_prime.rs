5ad78fcd9b4e893c4e8aea218f8db8c1abb15b13
jackyliu16 <18922251299@163.com>
2024-12-08 06:18:27 +0800

    topic1(S9): docs: add comment

--- topic1/solutiont9/src/spiral_prime.rs
+++ topic1/solutiont9/src/spiral_prime.rs
@@ -2,47 +2,52 @@ use std::fmt::format;
 use const_primes::Primes;
 
 /// 求对角线上素数的比例第一次低于某个比例时，螺旋数阵的边长是多少，此时有多少个素数？（比例不会小于 7%)
+/// 对角线上素数的比例第一次低于 threshold / 100 时返回当前边长与素数
+///
+/// # 输入值:
+///     threshold: 以百分比形式输入的比率, 当对角线上 素数数量/总对角点数量 小于此比例时返回
+///
+/// # 返回值:
+///     String: format!("{边长},{对角线上素数的个数}");
 pub fn min_edge_prime_num(threshold: u32) -> String {
     // 1, (2), _3_, 4, _5_, 6, _7_, 8, _9_, (10), 11, 12, _13_, 14, 15, 16, _17_
     // 18, 19, 20, _21_, 22, 23, 24, 25, (26), 27, 28, 29, 30, _31_, 32, ...
-    // 在距离中心一圈以外的地方，每隔 (边长 - 1)^2 则到达下一圈的起始点
-    // 自起始点前一个点起，每间隔(边长 - 2)则到达下一个边角。
+    // NOTE: 自起始点前一个点起，每间隔(2 * turn)则到达下一个边角。
 
     let mut turn = 1;
     let mut primes_cnt = 0;
 
     loop {
-        primes_cnt += how_many_prime_edge_in_this_turn(turn);
+        primes_cnt += how_many_prime_edges_in_this_turn(turn);
 
         if primes_cnt as f64 / (get_edge(turn) as f64) < threshold as f64 / 100_f64 {
-            return format!("{},{primes_cnt}", get_edge_len(turn));
+            return format!("{},{primes_cnt}", 1 + 2 * turn);
         }
 
         turn += 1;
     }
 }
 
-fn how_many_prime_edge_in_this_turn(this_turn: u32) -> u32 {
+/// 返回当前轮次中存在多少个 素数对角点
+fn how_many_prime_edges_in_this_turn(this_turn: u32) -> u32 {
     let last_turn = this_turn - 1;
-    let last_edge_len = get_edge_len(last_turn);
-    let mut cnt: u64 = last_edge_len as u64 * last_edge_len as u64;
+    let last_edge_len = 1 + 2 * last_turn;
+    let mut cnt: u64 = last_edge_len as u64 * last_edge_len as u64; // 上一圈的最后一个点的位置
     let mut prime_cnt = 0;
 
-    for i in 0..4 {
+    for _ in 0..3 { // 没有必要计算右下角
         cnt += 2 * this_turn as u64;
+        // 此处自己实现的 Miller-Rabin 素性测试算法性能不够, 无法在规定时间内完成计算
+        // 因此采用 const-primes 提供的高性能 Miller-Rabin 素性测试算法
         if const_primes::is_prime(cnt as u64) {
             prime_cnt += 1;
-            // dbg!(cnt);
         }
     }
 
     prime_cnt
 }
 
-fn get_edge_len(turn: u32) -> u32 {
-    1 + 2 * turn
-}
-
+/// 获取到某一轮次为止的对角点数量
 fn get_edge(turn: u32) -> u32 {
     match turn {
         0 => 1,
@@ -50,32 +55,29 @@ fn get_edge(turn: u32) -> u32 {
     }
 }
 
-// const PRIMES_CACHE: Primes<100000> = Primes::new();
-// /// 求素数
-// /// 当值小于 10000 时直接查表获取 cnt 是否是素数
-// /// 否则采用遍历法求解
-// fn is_prime(num: u64) -> bool {
-//     match PRIMES_CACHE.is_prime(num as u32) {
-//         Some(true) => true,
-//         Some(false) => false,
-//         None => {
-//             if num <= 1 {
-//                 return false;
-//             }
-//             for i in 2..=((num as f64).sqrt() as u64) {
-//                 if num % i == 0 {
-//                     return false;
-//                 }
-//             }
-//             true
-//         }
-//     }
-// }
 
-/// 计算直到当前 cnt 位置一共存在多少个对角点
+/*
+   ================================================== 
+   # 下面的部分与最终版本的实现无关, 仅作为曾经的实现而留存 #
+   ================================================== 
+*/
 
+
+/// 计算直到当前 cnt 数为止, 存在多少个符合标准的 对角点
+/// 
+/// # 输入:
+///     螺旋矩阵中对应点的数字
+/// 
+/// # Example:
+///     如果想要计算到 520 为止(包含520) 有多少个 对角点
+///     使用下面的函数, 会根据上一轮次的对角点数量 + 当前轮次的对角点数量
+///     计算得到当前 cnt 对应的对角点数量
+/// 
+/// ```rust
+/// how_many_edge(cnt, turn);
+/// ```
 #[inline(always)]
-#[deprecated]
+#[deprecated] // 本题没有必要针对于每一个对角点进行判断
 fn how_many_edge(cnt: u32, turn: u32) -> u32 {
     if cnt <= 9 || turn <= 1{ panic!("Not support cnt <= 9"); }
     let last_turn = turn - 1;
@@ -84,7 +86,7 @@ fn how_many_edge(cnt: u32, turn: u32) -> u32 {
     let remain_len = 1 + 2 * last_turn;
     let len_this = cnt - remain_len * remain_len;
     let this_edge = len_this / (this_len - 1);
-    remain_edge + this_edge as u32
+    remain_edge + this_edge
 }
 
 // mod tests {
