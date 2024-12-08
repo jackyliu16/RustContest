2029f17608c021f24c310cfd9b5e17f10a0d0fa9
jackyliu16 <18922251299@163.com>
2024-12-08 19:04:43 +0800

    topic!(S4): update comment

--- topic1/solutiont4/src/wade_giles_romanization.rs
+++ topic1/solutiont4/src/wade_giles_romanization.rs
@@ -3,27 +3,27 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
-use std::pin::pin;
 use unicode_normalization::UnicodeNormalization;
 
+// TODO: 可以将对应威妥玛拼音的映射关系与前面字形到拼音的转换表结合在一起,然后通过写入到某个特定的文件,用时再读取的方式降低重复构建的性能损耗
 pub fn converter(input: &str) -> String {
     dbg!(&input);
-    let kMandarin_map = load_pingyin_file("kMandarin.txt".into()).unwrap();
+    let kMandarin_map = load_pin_yin_file("kMandarin.txt".into()).unwrap();
     let mapping = load_mapping("mapping_pingyin_to_wade_giles".into()).unwrap();
-
     // dbg!(&kMandarin_map);
 
+    // 首先基于基础字形映射 将对应的中文内容转换成为对应的 拼音
     let pingyin = input.chars()
         // .map(|c| kMandarin_map[&c].clone())
         .map(|c| kMandarin_map.get(&c).unwrap_or(&" ".into()).clone())
         .collect::<Vec<_>>();
-
     // dbg!(&pingyin);
 
     if pingyin.len() <= 1 && pingyin[0] == " " {
         return "".into()
     }
 
+    // 然后再根据拼音与威妥玛拼音之间的映射关系, 将代表对应字符的拼音转换成为对应的威妥玛拼音
     // ("诸葛亮", "Chu ko liang"),
     // stage 1: zhu ge liang
     let mut res: Vec<_> = pingyin
@@ -31,6 +31,7 @@ pub fn converter(input: &str) -> String {
         .map(|words| mapping.get(words).unwrap_or(&" ".into()).clone())
         .collect();
 
+    // 根据答案的要求,将字符串的第一个 ASCII 转换成为大写表达形式
     if let Some(first) = res.first_mut() {
         let mut chars = first.chars();
         match chars.next() {
@@ -46,6 +47,8 @@ pub fn converter(input: &str) -> String {
     res.join(" ")
 }
 
+/// 从基于 CARGO_MANIFEST_DIR 的相对路径中加载 file_name 的文件
+/// 并根据文件内部的组织规则, 将每行中的一一映射转换成为对应的 HashMap 映射
 pub fn load_mapping<'a>(file_name: String) -> io::Result<HashMap<String, String>> {
     let manifest_path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let path = PathBuf::from(manifest_path).join(file_name);
@@ -69,7 +72,9 @@ pub fn load_mapping<'a>(file_name: String) -> io::Result<HashMap<String, String>
     Ok(map)
 }
 
-pub fn load_pingyin_file(file_name: String) -> io::Result<HashMap<char, String>> {
+/// 从基于 CARGO_MANIFEST_DIR 的相对路径中加载 file_name 的拼音文件
+/// 并根据文件内部的组织规则, 将每行的多个对应关系转换成为对应的 HashMap 映射
+pub fn load_pin_yin_file(file_name: String) -> io::Result<HashMap<char, String>> {
     let manifest_path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let path = PathBuf::from(manifest_path).join(file_name);
     let mut map: HashMap<char, String> = HashMap::new();
@@ -93,6 +98,8 @@ pub fn load_pingyin_file(file_name: String) -> io::Result<HashMap<char, String>>
             .collect::<String>();
 
         map.insert(unicode_to_char(&first).unwrap(), second.clone());
+        // 对于一行中存在多个映射结果的情况而言, 构建 yǐ 到不同的解释字形之间的相关关系
+        // U+2CE88: yǐ  # 𬺈  =>  U+9F6E
         if line.len() >= 6 {
             if let Some(val) = unicode_to_char(line[5]) {
                 map.insert(val, second);
