200f4e173e64c9e63e84294d983cc3232c6aa02f
jackyliu16 <18922251299@163.com>
2024-12-08 10:17:38 +0800

    topic1(S5): clean useless

--- topic1/solutiont5/src/main.rs
+++ topic1/solutiont5/src/main.rs
@@ -1,8 +1,5 @@
 mod retirement;
 fn main() {
-    // ("1995-12", "原法定退休年龄50周岁女职工", "2050-12,55,60"),
-    // ("1971-04", "原法定退休年龄55周岁女职工", "2026-08,55.33,4"),
-    // let res = retirement::retire_time("1995-12", "原法定退休年龄50周岁女职工");
-    let res = retirement::retire_time("1971-04", "原法定退休年龄55周岁女职工");
+    let res = retirement::retire_time("1971-04", "原法定退休年龄55周岁女职工"); // 2026-08,55.33,4
     println!("{res}");
 }
